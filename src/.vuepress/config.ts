5301c9230c67d5ec15eb68e3f6e1a37fcde5c456
gstorms <wangzx_2015@foxmail.com>
2023-07-19 12:39:45 +0800

    fix:change baseconfig

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/gbook/",
 
   lang: "zh-CN",
   title: "文档演示",
